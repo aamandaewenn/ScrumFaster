f220ba9528092d66bb5d581251853b26a353528a
Amanda <aamandaewenn@gmail.com>
2022-10-15 16:43:29 -0600

    created basic UI for the CreateNewUser window

--- src/main/java/ScrumFaster/CreateUserController.java
+++ src/main/java/ScrumFaster/CreateUserController.java
@@ -1,4 +1,32 @@
 package ScrumFaster;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 public class CreateUserController {
+
+    private String[] colours = {"red", "green"};
+
+    @FXML
+    private TextField NameTextField;
+
+    @FXML
+    private ColorPicker TaskColourPicker;
+
+    private void createUser()
+    {
+        //create new User object
+
+        //add user to ScrumBoardController.TeamMates
+
+        //close window
+
+    }
 }
+
+
