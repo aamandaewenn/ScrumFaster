040fbc214e1f687351c977e8a90f17904a96ea88
Muhammad Abdullah Khan <mak008@usask.ca>
2022-10-18 19:55:05 -0600

    Created the User Class and added UI elements to create a new user story. Updated UI formatting

--- src/main/java/ScrumFaster/User.java
+++ src/main/java/ScrumFaster/User.java
@@ -3,13 +3,61 @@ package ScrumFaster;
 import java.util.ArrayList;
 
 public class User {
-    ArrayList<UserStory> assignedStories;
-    String name;
-    String colour;
 
+    private ArrayList<UserStory> assignedStories;
+    private String name;
+    private String colour;
+
+    /**
+     * Class constructor.
+     *
+     * @param UserName The name for the new user.
+     * @param TaskColour The colour for the new user.
+     */
     public User(String UserName, String TaskColour)
     {
         name = UserName;
         colour = TaskColour;
+    }
+
+    // Getters and Setters.
+    public ArrayList<UserStory> getStories() {
+        return assignedStories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    // Class methods
+    /**
+     * Adds a UserStory object to the array list of assigned stories for this user.
+     *
+     * @param story The UserStory object to be assigned to the user.
+     */
+    public void assignStoryToThisUser(UserStory story){
+        this.assignedStories.add(story);
+    }
+
+
+    /**
+     * Removes a UserStory object from the array list of this users assigned stories.
+     *
+     * @param story The UserStory object to remove from this user.
+     */
+    public void removeStoryFromThisUser(UserStory story){
+        this.assignedStories.remove(story);
     }
 }
